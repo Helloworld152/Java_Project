055c7b061eb17d58363f21aaed10b39390f5ce8d
阮颖 <15224718167@163.com>
2019-01-25 17:56:41 +0800

    update

--- src/Date.java
+++ src/Date.java
@@ -18,6 +18,20 @@ public class Date {
 
     public String toString(){ return getMonth() + "/" + getDay() + "/" + getYear(); }
 
+    public boolean equals(Object x){
+
+        if (this == x) return true;
+        if (x == null) return false;
+        if (this.getClass() != x.getClass()) return false;
+
+        Date that = (Date) x;
+
+        if (this.day != that.day) return false;
+        if (this.month != that.month) return false;
+        if (this.year != that.year) return false;
+        return true;
+    }
+
     public static void main(String[] args) {
 
     }
---
+++ src/Stats.java
@@ -0,0 +1,9 @@
+import java.util.Queue;
+import java.util.Stack;
+
+public class Stats {
+
+    public static void main(String[] args) {
+        Stack<Double> numbers = new Stack<Double>();
+    }
+}
